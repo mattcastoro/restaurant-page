657d4d0ad61fde0f462441a9ca647b1ee9a6e7d5
Matt Castoro <matthewjcastoro@gmail.com>
2024-12-16 15:11:30 -0700

    Webpack project files setup
    Configure Webpack and associated files.  Set up import/export needs for
    modular js.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# restaurant-page
+This project will consist of building a restaurant homepage by leveraging what I have 
+previously learned about modules, npm, and Webpack.
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+import {  } from "./home.js";
+import {  } from "./menu.js";
+import {  } from "./about.js";
+
+// this is where the tab switching logic will be written
